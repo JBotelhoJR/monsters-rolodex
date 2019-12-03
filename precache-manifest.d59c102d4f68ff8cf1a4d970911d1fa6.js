55fc333c85ee43e0b23f6e27c7a91d46b1ec4b0b
Jose Botelho <botelho.j@outlook.com>
2019-12-03 16:54:31 -0500

    Updates

---
+++ precache-manifest.d59c102d4f68ff8cf1a4d970911d1fa6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff9b088a023fd0e9b6c102cbef4bb8ff",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "98eec73396eec3ae1082",
+    "url": "/monsters-rolodex/static/css/main.3bd54f75.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/monsters-rolodex/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "98eec73396eec3ae1082",
+    "url": "/monsters-rolodex/static/js/main.9ba804a2.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
